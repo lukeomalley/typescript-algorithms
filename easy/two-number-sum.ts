32ff637951f3f7ec2fac49dd8d24ebe152a466ce
Luke O'Malley <Lukeomalley95@gmail.com>
2020-12-20 02:23:47 -0500

    solved two number sum

--- easy/two-number-sum.ts
+++ easy/two-number-sum.ts
@@ -3,21 +3,36 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 /**
  * Two Number Sum
  *
- * <question-description>
+ * Write a function that takes in a non-empty array of distinct integers
+ * and an integer representing a target sum. If any two numbers in the
+ * input array sum up to the target sum, the function should return them
+ * in an array, in any order. If no two numbers sum up to the target sum,
+ * the function should return an empty array
  *
- * Input:
- * Output:
+ * Input: [1, 2, 1, 1, 1, 1, 7], 9
+ * Output: [2, 7]
  *
- * Time:
- * Space:
+ * Time: O(n)
+ * Space: O(n)
  */
-function twoNumberSum(args: any): any {
-  // Write solution here
+function twoNumberSum(numbers: number[], targetSum: number): number[] {
+  const seenNumbers: { [key: number]: boolean } = {};
+
+  for (const num of numbers) {
+    const remainingToTarget = targetSum - num;
+    if (remainingToTarget in seenNumbers) {
+      return [num, remainingToTarget];
+    }
+
+    seenNumbers[num] = true;
+  }
+
+  return [];
 }
 
 // =============================================================================
 // Tests
 // =============================================================================
 
-console.log(twoNumberSum(''));
-evalFunctionPerformance(twoNumberSum, '');
+console.log(twoNumberSum([1, 2, 1, 1, 1, 1, 7], 9));
+evalFunctionPerformance(twoNumberSum, [1, 2, 1, 1, 1, 1, 7], 9);
