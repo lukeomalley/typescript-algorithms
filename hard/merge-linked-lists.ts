94c4a75761eb6f8fa48129dc025b596b6521a90d
Luke O'Malley <Lukeomalley95@gmail.com>
2021-02-25 13:20:04 -0500

    working through solution to merge linked lists

--- hard/merge-linked-lists.ts
+++ hard/merge-linked-lists.ts
@@ -1,9 +1,9 @@
 import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 
 /**
- * Merge Linked Lists
+ * Merge Singly Linked Lists
  *
- * Write a function that takes in the heads of two Single Linked Lists that are in sorted
+ * Write a function that takes in the heads of two Singly Linked Lists that are in sorted
  * order, respectively. The function should merge the lists in place (i.e, it shouldn't
  * create a brand new list) and return the head of the merged list; the merged list should
  * be in sorted order.
@@ -15,18 +15,56 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
  * words, the heads will never be None / null
  *
  * Input:
- * Output:
+ *     headOne = 2 -> 6 -> 7 -> 8
+ *     headTwo = 1 -> 3 -> 4 -> 5 -> 9 -> 10
+ * Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10
  *
  * Time:
  * Space:
  */
-function mergeLinkedLists(args: any): any {
-  // Write solution here
+
+// This is an input class. Do not edit.
+export class LinkedList {
+  value: number;
+  next: LinkedList | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export function mergeLinkedLists(headOne: LinkedList, headTwo: LinkedList) {
+  // Write your code here.
+  while (headOne.next !== null) {
+    if (!headOne.value || !headTwo.value) {
+      return headOne;
+    }
+
+    if (headTwo.value <= headOne.value) {
+      // Merge the head of the list to merge into the list to iterate
+      const [newHeadOne, newHeadTwo] = mergeHeadTwoIntoHeadOne(headOne, headTwo);
+      headOne = newHeadOne;
+      headTwo = newHeadTwo;
+      continue;
+    }
+
+    headOne = headOne.next!;
+  }
+
+  return headOne;
+}
+
+function mergeHeadTwoIntoHeadOne(headOne: LinkedList, headTwo: LinkedList): [LinkedList, LinkedList] {
+  const newHeadOne = new LinkedList(headTwo.value);
+  newHeadOne.next = headOne;
+  headTwo = headTwo.next!;
+  return [newHeadOne, headTwo];
 }
 
 // =============================================================================
 // Tests
 // =============================================================================
 
-console.log(mergeLinkedLists(''));
-evalFunctionPerformance(mergeLinkedLists, '');
+console.log(mergeLinkedLists(new LinkedList(1), new LinkedList(2)));
+evalFunctionPerformance(mergeLinkedLists, new LinkedList(1), new LinkedList(2));
