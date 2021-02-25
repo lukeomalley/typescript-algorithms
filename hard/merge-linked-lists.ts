793c328bc38f631056f1735402b2e3a64d075e72
Luke O'Malley <Lukeomalley95@gmail.com>
2021-02-25 10:01:43 -0500

    generated merge linked lists problem

---
+++ hard/merge-linked-lists.ts
@@ -0,0 +1,23 @@
+import evalFunctionPerformance from '../lib/evalFunctionPerformance';
+
+/**
+ * Merge Linked Lists
+ *
+ * <question-description>
+ *
+ * Input:
+ * Output:
+ *
+ * Time:
+ * Space:
+ */
+function mergeLinkedLists(args: any): any {
+  // Write solution here
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+console.log(mergeLinkedLists(''));
+evalFunctionPerformance(mergeLinkedLists, '');
