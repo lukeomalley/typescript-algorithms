1e917fd213995d862f53ee42e896ed3add49a487
Luke O'Malley <Lukeomalley95@gmail.com>
2021-02-28 22:34:14 -0500

    filled out longest common subsequence

--- hard/longest-common-subsequence.ts
+++ hard/longest-common-subsequence.ts
@@ -3,10 +3,19 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 /**
  * Longest Common Subsequence
  *
- * <question-description>
+ * Write a function that takes in two strings and returns their longest common
+ * subsequence.
  *
- * Input:
- * Output:
+ * A subsequence of a string is a set of characters that aren't necessarily adjacent
+ * in the string but that are in the same order as they appear in the string. For
+ * instance, the characters ["a", "b", "c"] form a subsequence of the string "abcd",
+ * and so do the characters ["b", "d"]. Note that a single character in a string and
+ * the string itself are both valid subsequences of the string.
+ *
+ * You can assume that there will only be one longest subsequence.
+ *
+ * Input: "ZXVVYZW", "XKYKZPW"
+ * Output: ["X", "Y", "Z", "W"]
  *
  * Time:
  * Space:
