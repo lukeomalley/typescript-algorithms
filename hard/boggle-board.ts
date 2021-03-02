0333d3d0b9a518dfde1caca78af0b29614685364
Luke O'Malley <Lukeomalley95@gmail.com>
2021-03-01 23:58:30 -0500

    generated boggle board problem

---
+++ hard/boggle-board.ts
@@ -0,0 +1,23 @@
+import evalFunctionPerformance from '../lib/evalFunctionPerformance';
+
+/**
+ * Boggle Board
+ *
+ * <question-description>
+ *
+ * Input:
+ * Output:
+ *
+ * Time:
+ * Space:
+ */
+function boggleBoard(args: any): any {
+  // Write solution here
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+console.log(boggleBoard(''));
+evalFunctionPerformance(boggleBoard, '');
