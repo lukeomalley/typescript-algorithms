e5394e45cfe86a8d804bd9dd755129a496c3fc2c
Luke O'Malley <Lukeomalley95@gmail.com>
2021-03-08 11:06:24 -0500

    added continuous median problem

---
+++ hard/continuous-median.ts
@@ -0,0 +1,23 @@
+import evalFunctionPerformance from '../lib/evalFunctionPerformance';
+
+/**
+ * Continuous Median
+ *
+ * <question-description>
+ *
+ * Input:
+ * Output:
+ *
+ * Time:
+ * Space:
+ */
+function continuousMedian(args: any): any {
+  // Write solution here
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+console.log(continuousMedian(''));
+evalFunctionPerformance(continuousMedian, '');
