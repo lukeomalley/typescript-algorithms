7eea99cb9da250b2283b1aaa9ec9843ee7bb604f
Luke O'Malley <lukeomalley95@gmail.com>
2021-03-08 21:58:45 -0500

    generated shift linked list

---
+++ hard/shift-linked-list.ts
@@ -0,0 +1,23 @@
+import evalFunctionPerformance from '../lib/evalFunctionPerformance';
+
+/**
+ * Shift Linked List
+ *
+ * <question-description>
+ *
+ * Input:
+ * Output:
+ *
+ * Time:
+ * Space:
+ */
+function shiftLinkedList(args: any): any {
+  // Write solution here
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+console.log(shiftLinkedList(''));
+evalFunctionPerformance(shiftLinkedList, '');
