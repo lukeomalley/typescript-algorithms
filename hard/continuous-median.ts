2f0270da3aa3a4850e51f0f31db2d63d2d2fe9b2
Luke O'Malley <Lukeomalley95@gmail.com>
2021-03-08 19:52:54 -0500

    working through continuous median

--- hard/continuous-median.ts
+++ hard/continuous-median.ts
@@ -3,21 +3,68 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 /**
  * Continuous Median
  *
- * <question-description>
+ * Write a continuous median handler class that supports:
+ *    - The continuous insertion of numbers with an `insert` method
+ *    - The instant O(1) retrieval of the median of the numbers that have been
+ *      inserted thus far with a `getMedian` method
  *
- * Input:
- * Output:
+ * The median of a set of numbers is the "middle" number when the numbers are
+ * ordered from least to greatest. If there's an odd number of numbers in the set
+ * the median is the middle number. If there is an even number of numbers in the
+ * set then the median is the average of the two numbers in the middle
  *
- * Time:
+ * Sample Usage:
+ *   const medianHandler = new MedianHandler()
+ *   medianHandler.insert(5)
+ *   medianHandler.insert(10)
+ *   medianHandler.getMedian() ==> 7.5
+ *   medianHandler.insert(100)
+ *   medianHandler.getMedian() ==> 55
+ *
+ * Time: Insert: O(log(n))
  * Space:
  */
-function continuousMedian(args: any): any {
-  // Write solution here
+
+class ContinuousMedianHAndler {
+  median: number | null;
+
+  constructor() {
+    this.median = null;
+  }
+
+  insert() {
+    // Write solution here
+  }
+
+  getMedian() {
+    // Write solution here
+    return this.median;
+  }
+}
+
+class Heap {
+  heap: number[];
+  comparisonFunc: (a: number, b: number) => boolean;
+  length: number;
+
+  constructor(comparisonFunc: (a: number, b: number) => boolean, array: number[]) {
+    this.comparisonFunc = comparisonFunc;
+    this.heap = this.buildHeap(array);
+    this.length = this.heap.length;
+  }
+
+  buildHeap(array: number[]): number[] {
+    const firstParentIdx = Math.floor((array.length - 2) / 2);
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array);
+    }
+
+    return array;
+  }
 }
 
 // =============================================================================
 // Tests
 // =============================================================================
 
-console.log(continuousMedian(''));
-evalFunctionPerformance(continuousMedian, '');
+// console.log(continuousMedian(''));
