fb396ef7112a4bba7fd72d951981eeec10a246c6
Luke O'Malley <Lukeomalley95@gmail.com>
2021-05-26 14:29:56 -0400

    solved number of ways to traverse

--- medium/number-of-ways-to-traverse-graph.ts
+++ medium/number-of-ways-to-traverse-graph.ts
@@ -1,7 +1,7 @@
 import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 
 /**
- * Number Of Ways To Traverse Graph.ts
+ * Number Of Ways To Traverse Graph
  *
  * You're given two positive integers representing the width and height of a
  * grid-shaped, rectangular graph. Write a function that returns the number of
@@ -11,25 +11,68 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
  *
  * Note: You can assume that width * height >= 2
  *
- * input: width = 2, height = 3
+ * input: width = 4, height = 3
  * output: 10
  *
  *  |_|_|
  *  |_|_|
  *  |_|_|
  *
- * Time:
- * Space:
+ * Time: O(m * n)
+ * Space: O(m * n)
  */
 function numberOfWaysToTraverseGraph(width: number, height: number): number {
-  console.log(height, width);
+  return computeNumberOfWaysDP(height, width);
+}
 
-  return height * width;
+function numberOfWaysToTraverseGraphRecursive(width: number, height: number): number {
+  const cache = new Array(height).fill(0).map(() => new Array(width).fill(0));
+
+  return computeWaysMomo(height, width, cache);
+}
+
+// Memoized
+function computeWaysMomo(height: number, width: number, cache: number[][]): number {
+  const row = height - 1;
+  const col = width - 1;
+  if (cache[row][col] !== 0) {
+    return cache[row][col];
+  }
+
+  if (height === 1 || width === 1) {
+    cache[row][col] = 1;
+  } else {
+    cache[row][col] = computeWaysMomo(height - 1, width, cache) + computeWaysMomo(height, width - 1, cache);
+  }
+
+  return cache[row][col];
+}
+
+function computeNumberOfWaysDP(height: number, width: number): number {
+  const dp = new Array(height).fill(0).map(() => new Array(width).fill(0));
+
+  for (let i = 0; i < dp.length; i++) {
+    for (let j = 0; j < dp[0].length; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 1;
+      } else {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+  }
+
+  return dp[height - 1][width - 1];
 }
 
 // =============================================================================
 // Tests
 // =============================================================================
 
-console.log(numberOfWaysToTraverseGraph(2, 3));
-evalFunctionPerformance(numberOfWaysToTraverseGraph, 2, 3);
+console.log('Dynamic Programing:');
+console.log(numberOfWaysToTraverseGraph(4, 3));
+evalFunctionPerformance(numberOfWaysToTraverseGraph, 4, 3);
+console.log('\n');
+
+console.log('Recursive');
+console.log(numberOfWaysToTraverseGraphRecursive(4, 3));
+evalFunctionPerformance(numberOfWaysToTraverseGraphRecursive, 4, 3);
