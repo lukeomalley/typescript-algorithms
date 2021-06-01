9edf2339c1fddaeeea67a1e898b8fd89198ce079
Luke O'Malley <Lukeomalley95@gmail.com>
2021-06-01 10:31:51 -0600

    solved max sum submatrix

--- hard/maximum-sum-submatrix.ts
+++ hard/maximum-sum-submatrix.ts
@@ -3,21 +3,105 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 /**
  * Maximum Sum Submatrix
  *
- * <question-description>
+ * You're given a two-dimensional array (matrix) of potentially unequal height
+ * and width that's filled with integers. You're also given a positive integer
+ * size. Write a function tha tresutns the maximum sum that can be generated
+ * from a submatrix with dimensions size * size.
  *
  * Input:
- * Output:
+ *  [
+ *    [5, 3, -1, 5],
+ *    [-7, 3, 7, 4],
+ *    [12, 8, 0, 0],
+ *    [1, -8, -8, 2],
+ *  ]
+ *  size = 2
+ *
+ * Output: 18
+ *  [
+ *    [., ., ., .],
+ *    [., 3, 7, .],
+ *    [., 8, 0, .],
+ *    [., ., ., .],
+ *  ]
  *
  * Time:
- * Space:
+ * Space: O (n * m)
  */
-function maximumSumSubmatrix(args: any): any {
-  // Write solution here
+
+function maximumSumSubmatrix(matrix: number[][], size: number): number {
+  const sums = generateSumsArray(matrix);
+
+  let maxSum = Number.MIN_SAFE_INTEGER;
+
+  for (let i = size - 1; i < sums.length; i++) {
+    for (let j = size - 1; j < sums[i].length; j++) {
+      let currentSum = 0;
+
+      if (i === size - 1 && j === size - 1) {
+        // If top left cell
+        currentSum = sums[i][j];
+      } else if (i === size - 1 && j !== size - 1) {
+        // If on the top most row
+        currentSum = sums[i][j] - sums[i][j - size];
+      } else if (i !== size - 1 && j === size - 1) {
+        // If on the left most column
+        currentSum = sums[i][j] - sums[i - size][j];
+      } else {
+        currentSum = sums[i][j] - sums[i][j - size] - sums[i - size][j] + sums[i - size][j - size];
+      }
+
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+      }
+    }
+  }
+
+  return maxSum;
+}
+
+function generateSumsArray(matrix: number[][]): number[][] {
+  const sums = new Array(matrix.length).fill([]).map(() => new Array(matrix[0].length).fill(0));
+
+  // Fill in the first row
+  for (let i = 0; i < sums[0].length; i++) {
+    if (i === 0) {
+      sums[0][i] = matrix[0][i];
+    } else {
+      sums[0][i] = matrix[0][i] + sums[0][i - 1];
+    }
+  }
+
+  // Fill in the first column
+  for (let i = 0; i < sums.length; i++) {
+    if (i === 0) {
+      sums[i][0] = matrix[i][0];
+    } else {
+      sums[i][0] = matrix[i][0] + sums[i - 1][0];
+    }
+  }
+
+  // Fill out the rest of the array
+  for (let i = 1; i < sums.length; i++) {
+    for (let j = 1; j < sums[i].length; j++) {
+      sums[i][j] = matrix[i][j] + sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1];
+    }
+  }
+
+  return sums;
 }
 
 // =============================================================================
 // Tests
 // =============================================================================
+//
 
-console.log(maximumSumSubmatrix(''));
-evalFunctionPerformance(maximumSumSubmatrix, '');
+const testMatrix = [
+  [5, 3, -1, 5],
+  [-7, 3, 7, 4],
+  [12, 8, 0, 0],
+  [1, -8, -8, 2],
+];
+
+console.log(maximumSumSubmatrix(testMatrix, 2));
+evalFunctionPerformance(maximumSumSubmatrix, testMatrix, 2);
