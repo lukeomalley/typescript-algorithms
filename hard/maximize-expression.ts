2f8271d8ddfaf30d1aa0bd95f2c7c4a07c568433
Luke O'Malley <Lukeomalley95@gmail.com>
2021-06-01 20:58:00 -0600

    filled out maximize expression

--- hard/maximize-expression.ts
+++ hard/maximize-expression.ts
@@ -3,21 +3,34 @@ import evalFunctionPerformance from '../lib/evalFunctionPerformance';
 /**
  * Maximize Expression
  *
- * <question-description>
+ * Write a function that takes in an array of integers and returns
+ * the largest possible value for the expression:
  *
- * Input:
- * Output:
+ *     `array[a] - array[b] - array[c] - array[d]`
+ *
+ * Where a, b, c, and d are indices of the array and a < b < c < d.
+ * if the input array has fewer than 4 items, your function should
+ * return zero
+ *
+ * Input: [3, 6, 1, -3, 2, 7]
+ * Output: 4
  *
  * Time:
  * Space:
  */
-function maximizeExpression(args: any): any {
+function maximizeExpression(array: number[]): number {
   // Write solution here
+  if (array.length < 4) {
+    return 0;
+  }
+
+  return array[0];
 }
 
 // =============================================================================
 // Tests
 // =============================================================================
 
-console.log(maximizeExpression(''));
-evalFunctionPerformance(maximizeExpression, '');
+console.log(maximizeExpression([3, 6, 1, -3, 2, 7]));
+evalFunctionPerformance(maximizeExpression, [3, 6, 1, -3, 2, 7]);
+
