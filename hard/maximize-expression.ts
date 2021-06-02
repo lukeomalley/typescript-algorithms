3eff78c771e5d18b46eecc76a399bee1126ba165
Luke O'Malley <Lukeomalley95@gmail.com>
2021-06-01 20:49:00 -0600

    generated maximize-expression problem

---
+++ hard/maximize-expression.ts
@@ -0,0 +1,23 @@
+import evalFunctionPerformance from '../lib/evalFunctionPerformance';
+
+/**
+ * Maximize Expression
+ *
+ * <question-description>
+ *
+ * Input:
+ * Output:
+ *
+ * Time:
+ * Space:
+ */
+function maximizeExpression(args: any): any {
+  // Write solution here
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+console.log(maximizeExpression(''));
+evalFunctionPerformance(maximizeExpression, '');
