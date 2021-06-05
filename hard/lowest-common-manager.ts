5fd1a5f3a0553ad6bdcd1093d7e67d46569e37a5
Luke O'Malley <Lukeomalley95@gmail.com>
2021-06-05 07:24:17 -0600

    added lowest common manager problem

---
+++ hard/lowest-common-manager.ts
@@ -0,0 +1,23 @@
+import evalFunctionPerformance from '../lib/evalFunctionPerformance';
+
+/**
+ * Lowest Common Manager
+ *
+ * <question-description>
+ *
+ * Input:
+ * Output:
+ *
+ * Time:
+ * Space:
+ */
+function lowestCommonManager(args: any): any {
+  // Write solution here
+}
+
+// =============================================================================
+// Tests
+// =============================================================================
+
+console.log(lowestCommonManager(''));
+evalFunctionPerformance(lowestCommonManager, '');
